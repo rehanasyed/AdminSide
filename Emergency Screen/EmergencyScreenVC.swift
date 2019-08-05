88f53fb43dddcd4343e0b3542f53df55aa327513
Hasan Tahir <hasantahir444@gmail.com>
2019-08-06 01:54:55 +0500

    Map Integrated

--- Emergency Screen/EmergencyScreenVC.swift
+++ Emergency Screen/EmergencyScreenVC.swift
@@ -7,24 +7,86 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class EmergencyScreenVC: UIViewController {
-
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    var currentLocationMarker : GMSMarker = GMSMarker()
+    var userLocationMarker : GMSMarker = GMSMarker()
+    var currentLocation : CLLocationCoordinate2D = CLLocationCoordinate2D()
+    var userLocation : CLLocationCoordinate2D = CLLocationCoordinate2D()
+    let KarachiLocation =  CLLocation(latitude: 24.8607, longitude: 67.0011)
+    var currentZoomLevel : Float = 15.0
+    var currentType = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        mapView.delegate = self
+        
+        LocationHelper.sharedInstance.getCurrentLocation = {[weak self](location) in
+            self!.currentLocation = location.coordinate
+            let camera = GMSCameraPosition.camera(withLatitude: self!.KarachiLocation.coordinate.latitude, longitude: self!.KarachiLocation.coordinate.longitude, zoom: self!.currentZoomLevel)
+            self!.mapView.camera = camera
+            self!.addCurrentLocationMarker()
+        }
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension MapVC{
+    
+    func addMarkersInMap(Location location : CLLocationCoordinate2D, Type imageName : String){
+        let marker = GMSMarker(position: location)
+        marker.map = self.MapView
+        marker.icon = GoogleMapsManager.sharedInstance.setMarkerImage(image: UIImage(named: imageName)!, scaledToSize: CGSize(width: 50, height: 50))
+        
     }
-    */
+    
+    func addCurrentLocationMarker(){
+        currentLocationMarker.position = CLLocationCoordinate2D(latitude: KarachiLocation.coordinate.latitude, longitude: KarachiLocation.coordinate.longitude)
+        currentLocationMarker.appearAnimation = .pop
+        currentLocationMarker.map = self.MapView
+    }
+    
+    func addSelectedLocationMarker(Type type : String){
+        selectedLocationMarker.icon = GoogleMapsManager.sharedInstance.setMarkerImage(image: UIImage(named: type)!, scaledToSize: CGSize(width: 50, height: 50))
+        selectedLocationMarker.map = self.MapView
+    }
+    
+    
+    func adjustZoom (Radius rad : Float){
+        self.currentZoomLevel = 14.5 - (rad/7500)
+        self.MapView.camera = GMSCameraPosition.camera(withTarget: KarachiLocation.coordinate, zoom: self.currentZoomLevel)
+    }
+    
+    func zoomOut(){
+        self.MapView.camera = GMSCameraPosition.camera(withTarget: KarachiLocation.coordinate, zoom: 15)
+    }
+    
+}
 
+extension MapVC : GMSMapViewDelegate{
+    
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        
+        selectedLocationMarker = marker
+        GoogleMapsManager.sharedInstance.getDirections(Origin: self.KarachiLocation.coordinate, Destination: marker.position)
+        
+        GoogleMapsManager.sharedInstance.getDirections(Origin: self.KarachiLocation.coordinate, Destination: marker.position) { [weak self](routes) in
+            DispatchQueue.main.async {
+                self!.MapView.clear()
+                self?.addCurrentLocationMarker()
+                self?.addSelectedLocationMarker(Type: self!.currentType)
+                self!.zoomOut()
+                GoogleMapsManager.sharedInstance.drawPolyLineOnMap(Routes: routes, Map: self!.MapView)
+                
+            }
+            
+        }
+        return true
+    }
+    
 }
---
+++ HelperClasses/LocationHelper.swift
@@ -0,0 +1,54 @@
+//
+//  LocationHelper.swift
+//  EmergencyLocator
+//
+//  Created by Hasan Tahir on 15/02/2019.
+//  Copyright © 2019 Hasan Tahir. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationHelper : NSObject{
+    private static var _obj : LocationHelper? = nil
+    var locationManager: CLLocationManager!
+    var getCurrentLocation :  ((_ location : CLLocation)->Void)? = nil
+    
+    override init() {
+        super.init()
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            //locationManager.startUpdatingHeading()
+        }
+    }
+    
+    class var sharedInstance:LocationHelper{
+        get{
+            if _obj == nil{
+                _obj = LocationHelper()
+            }
+            let lockQueue = DispatchQueue(label: "_obj")
+            return lockQueue.sync{
+                return _obj!
+            }
+        }
+    }
+}
+
+extension LocationHelper : CLLocationManagerDelegate{
+    
+  
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        getCurrentLocation!(userLocation)
+        locationManager.stopUpdatingLocation()
+        
+    }
+    
+    
+    
+}
